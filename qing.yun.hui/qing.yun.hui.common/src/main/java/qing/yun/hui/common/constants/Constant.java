0cb9e7c45904d519058799cead43072a44218667
qingyunhui <280672161@qq.com>
2016-11-20 22:38:05 +0800

    update

--- qing.yun.hui/qing.yun.hui.common/src/main/java/qing/yun/hui/common/constants/Constant.java
+++ qing.yun.hui/qing.yun.hui.common/src/main/java/qing/yun/hui/common/constants/Constant.java
@@ -12,4 +12,6 @@ public class Constant {
 	public static final String DECIMAL_STR="0123456789";//十进制
 	
 	public static final String MY_SESSION_INFO="mySessionInfo";
+	
+	public static final String USER_INFO_SESSION="userInfoSession";
 }
