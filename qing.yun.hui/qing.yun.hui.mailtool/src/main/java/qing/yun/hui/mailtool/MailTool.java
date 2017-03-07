d25f0f5a600e8ba5d2fde607656d1e6828fe6edc
卿云辉 <qingyunhui@zuozh.com>
2017-03-07 13:32:26 +0800

    邮箱密码加密

--- qing.yun.hui/qing.yun.hui.mailtool/src/main/java/qing/yun/hui/mailtool/MailTool.java
+++ qing.yun.hui/qing.yun.hui.mailtool/src/main/java/qing/yun/hui/mailtool/MailTool.java
@@ -18,7 +18,7 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.internet.MimeUtility;
 
-import qing.yun.hui.common.utils.StringUtil;
+import qing.yun.hui.common.utils.RSAUtil;
 
  
 public class MailTool {
@@ -26,7 +26,13 @@ public class MailTool {
 	private static String USER_KEY="user";
 	private static String PASSWORD_KEY="password";
 	private static String MAIL_HOST_KEY="mail.host";
-	private static String ADDRESSES_KEY="addresses";
+	
+	//公钥key
+	protected static final String _publicKey="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCWbYw374WwDCe+RmMFrFXYmiWz9W0AVKvCaVANsEKxXWPUDIgMQPMKeQB/0Flryt0RFlFcfaVCljbVhgwRDEEAe8WYABxXHiEfhoRhWUoVSx5Vu50fOnk4mZUM2Wo2jnBDR/ZU/wZh9YpbTYImy2cNGh5kqNk/ZRm4EI0yq1lgUwIDAQAB";
+    
+	//私钥key
+	protected static final String _privateKey="MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJZtjDfvhbAMJ75GYwWsVdiaJbP1bQBUq8JpUA2wQrFdY9QMiAxA8wp5AH/QWWvK3REWUVx9pUKWNtWGDBEMQQB7xZgAHFceIR+GhGFZShVLHlW7nR86eTiZlQzZajaOcENH9lT/BmH1iltNgibLZw0aHmSo2T9lGbgQjTKrWWBTAgMBAAECgYBGfl/Pdre3oHqfeGy/I9u2/cXdZ44FMaisGp4hTBB0/cbigFzhIS7EIaBSRVkiYpbmgwbtyRPA+JQJzB+rfYqX7Qb42t4AaeKiZUMnqGh73gJ/746DqCl9iMg78Vt3fCVzRd41WJmb2nR+C/ftjZOIybL8TPzDh5BVgEZdYwH9AQJBAOcKJvyaQYQQWj8YmzwC4/xnZFg/yVZW5iwGe363Et3gp+gdMvH/OWDI2ekYclsSSSlnEE7MYcSBVXgeakWTdjECQQCmrewY20e/yIQhKi62QAWTR4fxjEEYxYRa+H1E5iVLF+n3m9LXFtvcThI54qgwknY1s+DsA0JIvXxlp2rDAKnDAkEAtC7nvuLyq3MxXrnq/v9mHIfp6yU7+TjmY3lewLE4Zo/5gv2aqL6aWg/qWm61aK83HbICNuIQcXdIV9iomsBpUQJAU095Q89OpIBf8oe4A2YqozvMqiIVY4FOL34mduql10vjqNyc3N9TE2F+YKp2sJh0N1Fqae0TE3KKm/C7Py60qwJABss3wYwjsMjWkl6CGP5h+Jm7pIjM0AhWu5Dm+779twjhGuxPmXqpkOmzagsAPqWShJAgINeuPDfNJgFfzsUZHg==";
+
     
 	/**
 	 * @param prop【N】  可以为null，如果为null，则会new 一个Properties.
@@ -46,17 +52,19 @@ public class MailTool {
             session.setDebug(true);//开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
             ts = session.getTransport(); //2、通过session得到transport对象
             //3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
-            ts.connect(prop.getProperty(MAIL_HOST_KEY).trim(), prop.getProperty(USER_KEY).trim(), prop.getProperty(PASSWORD_KEY).trim());
+            String password=getDecryptionPassword(_privateKey, prop.getProperty(PASSWORD_KEY).trim());
+            ts.connect(prop.getProperty(MAIL_HOST_KEY).trim(), prop.getProperty(USER_KEY).trim(), password);
             MimeMessage message = new MimeMessage(session);//4、创建邮件
             message.setFrom(prop.getProperty(USER_KEY).trim()); //指明邮件的发件人
             //指明邮件的收件人，现在发件人和收件人是一样的，那就是自己给自己发
-            String[] addresses=!StringUtil.isEmpty(sendEmails)?sendEmails:prop.getProperty(ADDRESSES_KEY).trim().split(",");
+            String[] addresses=sendEmails;
             InternetAddress[] internetAddress = new InternetAddress[addresses.length];
             for(int i=0; i<internetAddress.length; i++){
             	internetAddress[i] = new InternetAddress(addresses[i].trim());
             }
             message.setRecipients(Message.RecipientType.TO, internetAddress);//设置接收者
             message.setSubject(subject);//邮件的标题
+            //这里加上了:outlook的外壳，防止被邮箱认为是垃圾邮箱的处理.
             message.setHeader("X-Mailer","Microsoft Outlook Express 6.00.2900.2869");
             message.setSentDate(new Date());//发送时间  
             message.setContent(content, "text/html;charset=UTF-8");//邮件的文本内容
@@ -77,7 +85,40 @@ public class MailTool {
     	return success;
     }
     
-    public String beginSendMail(String userTheme,String userAddressee,String userMain){
+    /**
+     * 发送邮件
+     * @param subject 主题
+     * @param content 内容
+     * @param sendEmails 接收对象(邮箱)
+     * */
+    public static Boolean sendMail(String subject,String content,String[] sendEmails) throws Exception {
+    	Properties prop = new Properties();
+    	Boolean success=false;
+        prop.load(Main.class.getResourceAsStream("/mail.properties"));
+		try {
+			success=sendTextMail(prop,subject,content,sendEmails);
+		} catch (Exception e) {
+			sendTextMail(prop,subject,content,sendEmails);
+			throw new Exception("邮件发送失败，失败原因："+e);
+		}
+		return success;
+    }
+    
+    /**
+     * <p>获取解密后的密码</p>
+     * @param privateKey 私钥
+     * @param encryptionPassword 加密后的密码
+     * @return 解密后的密码
+     * */
+    private static String getDecryptionPassword(String privateKey,String encryptionPassword){
+    	try {
+    		return RSAUtil.decryptionByPrivateKey(privateKey, encryptionPassword);
+		} catch (Exception e) {
+			return encryptionPassword;
+		}
+    }
+    
+    protected String beginSendMail(String userTheme,String userAddressee,String userMain){
 		InternetAddress[] popAddressList = null;
 		String smtpServer = "smtp.163.com";
 		String SmtpAddress = "289341714@qq.com";
@@ -159,31 +200,4 @@ public class MailTool {
 		}
 		return "beginSendMail";
 	}
-	
-    /**
-     * 发送邮件
-     * @param subject 主题
-     * @param content 内容
-     * @param sendEmails 接收对象(邮箱)
-     * */
-    public static Boolean sendMail(String subject,String content,String[] sendEmails) throws Exception {
-    	Properties prop = new Properties();
-    	Boolean success=false;
-        prop.load(Main.class.getResourceAsStream("/mail.properties"));
-		try {
-			success=sendTextMail(prop,subject,content,sendEmails);
-		} catch (Exception e) {
-			sendTextMail(prop,subject,content,sendEmails);
-			throw new Exception("邮件发送失败，失败原因："+e);
-		}
-		return success;
-    }
-    
-    public static void main(String[] args){
-    	try {
-			sendMail("what?", "没收到？", new String[]{"274788664@qq.com","280672161@qq.com"});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    }
 }
