6da431196ea379aa27922314cd1dbb7328861093
卿云辉 <qingyunhui@zuozh.com>
2017-03-21 17:38:26 +0800

    添加anno

---
+++ qing.yun.hui/qing.yun.hui.common/src/main/java/qing/yun/hui/common/annotations/ActionAnno.java
@@ -0,0 +1,23 @@
+package qing.yun.hui.common.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/***
+ ** @category 请用一句话来描述其用途...
+ ** @author qing.yunhui
+ ** @email: 280672161@qq.com
+ ** @createTime: 2017年3月21日下午5:09:14
+ **/
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ActionAnno {
+	
+	/**执行动作名称:比如（增，删，改，查）*/
+	String name() default "";
+
+}
