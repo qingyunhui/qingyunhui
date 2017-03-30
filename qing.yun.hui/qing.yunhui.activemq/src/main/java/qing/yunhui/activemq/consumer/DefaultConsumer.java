332b9cca82e01a58a236f70047c4fca9da9d2861
卿云辉 <qingyunhui@zuozh.com>
2017-03-30 13:43:50 +0800

    activemq project

---
+++ qing.yun.hui/qing.yunhui.activemq/src/main/java/qing/yunhui/activemq/consumer/DefaultConsumer.java
@@ -0,0 +1,10 @@
+package qing.yunhui.activemq.consumer;
+/***
+ ** @category 请用一句话来描述其用途...
+ ** @author qing.yunhui
+ ** @email: 280672161@qq.com
+ ** @createTime: 2017年3月30日下午2:00:23
+ **/
+public class DefaultConsumer {
+
+}
