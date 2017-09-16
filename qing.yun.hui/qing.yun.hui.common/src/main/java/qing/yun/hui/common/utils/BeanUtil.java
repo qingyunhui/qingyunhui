01910d741976c9582523f3a99b08b15b56d671e8
qingyunhui <qingyunhui@smoney.cc>
2017-09-16 14:35:47 +0800

    添加公共方法

--- qing.yun.hui/qing.yun.hui.common/src/main/java/qing/yun/hui/common/utils/BeanUtil.java
+++ qing.yun.hui/qing.yun.hui.common/src/main/java/qing/yun/hui/common/utils/BeanUtil.java
@@ -4,9 +4,13 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 
 import qing.yun.hui.common.enums.ICommonEnum;
 
@@ -165,4 +169,20 @@ public class BeanUtil {
 		}
 		return tmpobj;
 	}
+	
+	/**
+	 * 遍历给定的对象，找出属性为null的字段
+	 * */
+	public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<String>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
 }
